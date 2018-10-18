18568d775fd4f6ab3b2e7b4ba92a1f56e47b4e75
aluno <lucasalves@gmail.com>
2018-10-17 21:50:57 -0300

    add bootstrap e bugs

--- src/Controller/PaginaController.php
+++ src/Controller/PaginaController.php
@@ -7,25 +7,46 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-class PaginaController extends AbstractController
-{
+class PaginaController extends AbstractController {
+
     /**
-     * @Route("/pagina", name="pagina")
+     * @Route("/", name="index")
      */
-    public function index()
-    {
+    public function index() {
         return $this->render('pagina/index.html.twig', [
-            'controller_name' => 'PaginaController',
+                    'controller_name' => 'PaginaController',
         ]);
     }
-    
+
     /**
      * @Route("/contato", name="contato")
      */
-    public function contato(Request $request)
-    {
-        
+    public function contato(Request $request) {
+
         return $this->render('pagina/contato.html.twig');
     }
-    
+
+    /**
+     * @Route("/reservar", name="reservar")
+     */
+    public function reservar() {
+        return $this->render('pagina/reservar.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login() {
+
+        return $this->render('pagina/login.html.twig');
+    }
+
+    /**
+     * @Route("/admin",name="admin")
+     */
+    public function admin() {
+
+        return $this->render('pagina/admin.html.twig');
+    }
+
 }
